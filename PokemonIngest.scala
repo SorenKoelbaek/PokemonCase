57defc2641f3cea9b82859096fd4aca91a5424b4
SorenKoelbaek <SorenKoelbaek>
2022-05-13 11:11:24 +0000

    Refactored Ingestion
    I made a new ingestion Engine
    Much more effective, but I removed the pseudonymisation according to the solution design.
    
    Files are partitioned by date and available in the Raw/Pokemon area

--- PokemonIngest.scala
+++ PokemonIngest.scala
@@ -5,7 +5,6 @@ import org.apache.spark.sql.types.{ArrayType, IntegerType, StringType, StructFie
 import okhttp3.{Headers, OkHttpClient, Request, Response}
 
 
-
 val appName: String = "PokemonCase"
 val spark = SparkSession
   .builder()
@@ -33,9 +32,8 @@ val PokemonIteraterSchema: StructType = (new StructType)
 var uri = "https://pokeapi.co/api/v2/pokemon";
 var hasnext = true;
 
-//Declare a dataframe to hold our collected object --> maybe use a list instead?
-var PokemonIterater_df = spark.createDataFrame(spark.sparkContext
-      .emptyRDD[Row], PokemonIteraterObject)
+//We create a list object of pokemonURLS to iterate through later
+var pokemonURLlist: List[String] = List()
 
 while(hasnext)
 {
@@ -44,11 +42,10 @@ while(hasnext)
   
   var nextUrl = source_df.select(col("next")).first().getString(0);
   
-//Explode the Pokemon result object alongside the attributed URL
-  val delta_df =  source_df.select(explode(col("Results")).alias("pokemons"))
-      .select(col("pokemons.name"), col("pokemons.url"))
+//Explode the Pokemon result object and select the attributed URL and add to the list
+  pokemonURLlist = List.concat(pokemonURLlist, source_df.select(explode(col("Results")).alias("pokemons"))
+      .select("pokemons.url").map(f=>f.getString(0)).collect.toList)
  
-  PokemonIterater_df = PokemonIterater_df.union(delta_df);
   
   //Check if the next URL is set, if not stop the while loop
   if(nextUrl != null)
@@ -62,7 +59,7 @@ while(hasnext)
 
 }
 
-PokemonIterater_df.cache()
+
 
 case class RestAPIRequest (url: String)
 
@@ -99,49 +96,48 @@ class HttpRequest {
 // COMMAND ----------
 
 
-//Now we loop through all pokemons in our Iterator-list to extract information on each pokemon and save that into our Raw filesystem alongside a watermark and extraction URL.
-import org.apache.spark.sql.functions.current_timestamp   
 
-case class pokemonUrl(name:String, url:String)
+//Now we loop through all pokemons in our Iterator-list to extract information on each pokemon and save that into our Raw filesystem alongside a watermark.
+import org.apache.spark.sql.functions.current_timestamp   
+import org.apache.spark.sql.functions.to_date
 
 import java.sql.Timestamp
 import java.text.SimpleDateFormat
 import java.time.Instant
 
-//We store our Extracted data, the only manipulation we've done so far is to identify and split our PII data into two different stores, in order for our Pseudonymisation to work!
-val now: Timestamp = Timestamp.from(Instant.now())
-val date = new SimpleDateFormat("yyyyMMdd").format(now)
+val LandinDir = "/FileStore/raw/Pokemon/"
 
-val DirPseudo = "/FileStore/raw/Pokemon_Pseudonymised/".concat(date).concat("/")
-val dirIdentifier = "/FileStore/raw/Pokemon_Identifier/".concat(date).concat("/")
+var responseList = new scala.collection.mutable.ListBuffer[String]()
 
-
-PokemonIterater_df.as[pokemonUrl].take(PokemonIterater_df.count.toInt).foreach(t => 
+for (PokeUrl <- pokemonURLlist )
 {
- val uri = t.url
- val name = t.name
+     val httpRequest = new HttpRequest;
+    responseList += httpRequest.ExecuteHttpGet(PokeUrl)                              
+}
 
- val httpRequest = new HttpRequest;
- val source_df = spark.read.json(Seq(httpRequest.ExecuteHttpGet(uri)).toDS())
-  //We split our data into two, (identifiable or not) and use sha_256 to encrypt our unique pokemon name.
-  //I drop the moves variable as it is huge.
- val identifiable_df = source_df.select(col("name"), col("id"), col("species"), col("forms")).withColumn("Identifier",sha2(col("name"),256));
- val pseudonymised_df = source_df.withColumn("Identifier",sha2(col("name"),256)).drop("name").drop("id").drop("species").drop("forms").drop("moves");
+val pokemons_ds = responseList.toDS()
+val Pokemons_df = spark.read.json(pokemons_ds)
 
-  identifiable_df.withColumn("current_timestamp",current_timestamp().as("current_timestamp")).write.mode("append").format("json").save(dirIdentifier);
-  pseudonymised_df.withColumn("current_timestamp",current_timestamp().as("current_timestamp")).write.mode("append").format("json").save(DirPseudo);
- 
-})
 
-//Rewrite again, to increase performance of the data dump -- way too slow:
-  //Create a single dataframe from the responses and then write that to disk.
-  //Use partition in the streamwriter to partition by timestamp, instead of manually creating the directory.
+  Pokemons_df
+    .withColumn("current_timestamp",current_timestamp())
+    .withColumn("date",to_date(col("current_timestamp")))
+    .write.mode("overwrite")
+    .partitionBy("date")
+    .format("json")
+    .save(LandinDir);
+  
 
 
 
 // COMMAND ----------
 
-//dbutils.fs.rm("/FileStore/raw/Pokemon_Pseudonymised/",true)
-//dbutils.fs.rm("/FileStore/raw/Pokemon_Identifier/",true)
+
+val df = spark.read.json("/FileStore/raw/Pokemon/")
+
+//dbutils.fs.rm("/FileStore/raw/Pokemon/",true)
+
+
+// COMMAND ----------
 
 
